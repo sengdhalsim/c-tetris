b63a1d47370b6fd1371b4fc8890f07be4b3703ee
sengdhalsim <sengdhalsim@gmail.com>
2016-02-18 14:09:33 +0100

    Create block.cpp

---
+++ block.cpp
@@ -0,0 +1,17 @@
+#include "headers/block.h"
+
+int Block::GetX () {
+  return _x;
+}
+
+int Block::GetY () {
+  return _y;
+}
+
+void Block::SetX (int x) {
+  _x = x;
+}
+
+void Block::SetY (int y) {
+  _y = y;
+}
